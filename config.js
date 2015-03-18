e2086a80d2180ba46ca40126fe9a5197786145c2
Gaeulbyul <aeiou256@naver.com>
2015-03-18 20:42:37 +0900

    fix r10 port bind error

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var env = process.env;
 
 module.exports = {
-	port: env.ANION_PORT || 8081,
+	port: env.PORT || env.ANION_PORT || 8081,
 	env: env.ANION_ENV || 'development',
 	useragent: env.ANION_USERAGENT || 'Ani-ON Crawler - TestVersion',
 	database: {
