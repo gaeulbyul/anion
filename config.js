c63f320a693f56224fffe6600e159a3cc1265d81
Gaeulbyul <aeiou256@naver.com>
2015-03-22 13:22:11 +0900

    remove "TestVersion" on useragent

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var env = process.env;
 
 module.exports = {
 	port: env.PORT || env.ANION_PORT || 8081,
-	env: env.ANION_ENV || 'development',
-	useragent: env.ANION_USERAGENT || 'Ani-ON Crawler - TestVersion',
+	env: env.ANION_ENV || 'production',
+	useragent: env.ANION_USERAGENT || 'Ani-ON Crawler',
 	database: {
 		url : env.DATABASE_URL,
 		host: env.ANION_DB_HOST,
